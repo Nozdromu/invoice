14c8753d9f5cdfa2a7f2e5b28bc31ac80c7561d7
Nozdromu <yileiding1990@gmail.com>
2024-04-20 15:57:16 -0700

    update

--- server.js
+++ server.js
@@ -112,13 +112,13 @@ app.get('/trips', (req, res) => {
 
 app.get('/gettrips', (req, res) => {
     unfinishtrip = []
-    Object.entries(data).forEach(element => {
-        if (element[1].state * 1 === 1) {
-            unfinishtrip.push(element[1]);
-        }
-    });
-    console.log(unfinishtrip)
-    res.send({ trips: unfinishtrip })
+    // Object.entries(data).forEach(element => {
+    //     if (element[1].state * 1 === 1) {
+    //         unfinishtrip.push(element[1]);
+    //     }
+    // });
+    // console.log(unfinishtrip)
+    res.send({ trips: data })
 })
 
 app.get('/finish', (req, res) => {
