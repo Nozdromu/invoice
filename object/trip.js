db4c9a2a601c35e224e49182c18ea7fd431052f3
Nozdromu <yileiding1990@gmail.com>
2024-05-05 07:51:46 +0000

    update

---
+++ object/trip.js
@@ -0,0 +1,6 @@
+function trip(){
+    var _trip={
+        user:{},
+        trip_info:{}
+    }
+}
---
+++ object/trip_record.js
@@ -0,0 +1,11 @@
+function trip_record(){
+    var record={
+        id:0,
+        nickname:"",
+        billname:"",
+        invoice:"",
+        datetime:"",
+        date:"",
+        time:"",
+    }
+}
